bf6ac49c4a83cba48d33014cf3bc590dafde02af
mrcoconuat <28863780+MrCocoNuat@users.noreply.github.com>
2024-08-07 19:55:36 -0400

    architecture before function!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import * as hullDefinitions from './specs/hulls.json';
+import Select from 'react-select'
+import { useState } from 'react';
 
 function App() {
+  const [hull, setHull] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Select options={Array.from(hullDefinitions).map(hullDefinition => ({value: hullDefinition, label: hullDefinition.name}))} onChange={(hullDefinition) => {console.log(hullDefinition); setHull(hullDefinition.value)}}/>
+        <p>{hull && hull.maxMass}</p>
       </header>
     </div>
   );
