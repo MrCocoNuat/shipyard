a1d7fea0308263fd5267d7394b2a392fccba23bd
mrcoconuat <28863780+MrCocoNuat@users.noreply.github.com>
2024-08-10 23:37:15 -0400

    start distinguishing ships from hulls

--- src/component/Selector.js
+++ src/component/Selector.js
@@ -5,25 +5,27 @@ import * as specialDefinitions from '../specs/special.json';
 import { useState } from 'react';
 import Select from 'react-select';
 
+function emptyShip(hullDefinition){
+  return hullDefinition == null ? null : {hull: hullDefinition, equipment: [], specialEffects: []};
+}
+
 function Selector() {
-    const [hull, setHull] = useState(null);
-    // hull will need to be distinctified from shipDefinition - and this will need to be shipDefinition
-    //  hullDefinition = {name, maxmass, slots, ...}
-    // shipDefinition = {hull, mass, weapons, armor, specials, ...}
+    const [ship, setShip] = useState(null);
+
     return (
       <div className="selector">
         <div className="hull-selector">
-          <Select options={Array.from(hullDefinitions).map(hullDefinition => ({value: hullDefinition, label: hullDefinition.name}))} onChange={(hullDefinition) => setHull(hullDefinition.value)}/>
-          <p>{hull && hull.maxMass}</p>
+          <Select options={Array.from(hullDefinitions).map(hullDefinition => ({value: hullDefinition, label: hullDefinition.name}))} onChange={(hullDefinition) => setShip(emptyShip(hullDefinition.value))}/>
+          <p>{ship && ship.hull.maxMass}</p>
         </div>
         <div className="weapon-selector">
-            {new Array(hull?.slots.weapon || 0).fill(0).map((_, i) => <Select options={Array.from(weaponDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
+            {new Array(ship?.hull.slots.weapon || 0).fill(0).map((_, i) => <Select options={Array.from(weaponDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
         </div>
         <div className="armor-selector">
-            {new Array(hull?.slots.armor || 0).fill(0).map((_, i) => <Select options={Array.from(armorDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
+            {new Array(ship?.hull.slots.armor || 0).fill(0).map((_, i) => <Select options={Array.from(armorDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
         </div>
         <div className="special-selector">
-            {new Array(hull?.slots.special || 0).fill(0).map((_, i) => <Select options={Array.from(specialDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
+            {new Array(ship?.hull.slots.special || 0).fill(0).map((_, i) => <Select options={Array.from(specialDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
         </div>
       </div>
     );
