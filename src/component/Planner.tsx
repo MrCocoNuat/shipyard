594d7d44fe65b071d6fea2acd9bddf6a17e1d9d7
mrcoconuat <28863780+MrCocoNuat@users.noreply.github.com>
2024-08-27 21:20:29 -0400

    clean up

--- src/component/Planner.tsx
+++ src/component/Planner.tsx
@@ -20,18 +20,7 @@ function Selector() {
             <div>{`MASS: ${massOf(ship)}/${ship.hull.atUpgrade[ship.upgrade]?.maxMass}`}</div>
           </div>}
           <EquipmentSelector ship={ship} setShip={setShip}/>
-          {/* <Select options={Array.from(hullDefinitions).map(hullDefinition => ({value: hullDefinition, label: hullDefinition.name}))} onChange={(hullDefinition) => setShip(emptyShip(hullDefinition.value))}/> */}
-          {/* <p>{ship && ship.hull.maxMass}</p> */}
         </div>
-        {/* <div className="weapon-selector">
-            {new Array(ship?.hull.slots.weapon || 0).fill(0).map((_, i) => <Select options={Array.from(weaponDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
-        </div>
-        <div className="armor-selector">
-            {new Array(ship?.hull.slots.armor || 0).fill(0).map((_, i) => <Select options={Array.from(armorDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
-        </div>
-        <div className="special-selector">
-            {new Array(ship?.hull.slots.special || 0).fill(0).map((_, i) => <Select options={Array.from(specialDefinitions).map(weaponDefinition => ({value: weaponDefinition, label: weaponDefinition.name}))} key={i}/>)}
-        </div> */}
       </div>
     );
   }
